75d16b9058316fb68ed72df72c4aec8691a3aca8
Mmackz <matthewmackenz@gmail.com>
2022-01-19 12:00:15 -0800

    complete task 2

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const things = require("./things");
 const app = express();
 
 app.route("/").get((req, res) => res.send("Hello Express"));
-
 app.use("/things", things);
+app.get("/:id", (req, res) => res.send("The id is " + req.params.id))
 app.get("/json", (req, res) => {
    res.send({"hi": "hi"})
 })
