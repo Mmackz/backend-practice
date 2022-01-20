5f34b04ea7ba5d8c9690ff59bc9a2dd54346159c
Mmackz <matthewmackenz@gmail.com>
2022-01-19 21:38:15 -0800

    import/export modules practice

---
+++ basic-practice/test-express.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const square = require("./squares");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+   console.log(`App listening on port ${port} \n 4x4 square perimeter: ${square.area(8)}`)
+);
